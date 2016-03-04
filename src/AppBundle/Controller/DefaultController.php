b6a6b7f8522b67722d4d10669b995cda1c202210
Yohualli <tuguali19@ciencias.unam.mx>
2016-03-04 14:02:51 -0600

    agregar vistas y cambio de estructuracion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,17 +11,63 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('AppBundle::index.html.twig');
+    public function indexAction(Request $request){
+        return $this->render('@App/pages/index.html.twig');
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projectsAction(){
+        return $this->render('@App/pages/projects.html.twig');
+    }
+
+    /**
+     * @Route("/what-we-offer", name="what_we_offer")
+     */
+    public function whatWeOfferAction(){
+        return $this->render('@App/pages/whatWeOffer.html.twig');
+    }
+
+    /**
+     * @Route("/packages", name="packages")
+     */
+    public function packagesAction(){
+        return $this->render('@App/pages/packages.html.twig');
+    }
+
+    /**
+     * @Route("/promos", name="promos")
+     */
+    public function promosAction(){
+        return $this->render('@App/pages/promos.html.twig');
+    }
+
+    /**
+     * @Route("/how-to-buy", name="how_to_buy")
+     */
+    public function howToBuyAction(){
+        return $this->render('@App/pages/howToBuy.html.twig');
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blogAction(){
+        return $this->render('@App/pages/blog.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(){
+        return $this->render('@App/pages/contact.html.twig');
     }
 
     /**
      * @Route("/about-us", name="about_us")
      */
-    public function aboutUsAction(Request $request)
-    {
-        return $this->render('AppBundle::aboutUs.html.twig');
+    public function aboutUsAction(Request $request){
+        return $this->render('@App/pages/aboutUs.html.twig');
     }
 }
