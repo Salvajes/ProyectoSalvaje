6c8f7cff1e8ca0de15d56380447391c0ea2a1902
Yohualli <tuguali19@ciencias.unam.mx>
2016-03-29 13:32:27 -0600

    reacomodo de pagina

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,41 +16,6 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/projects", name="projects")
-     */
-    public function projectsAction(){
-        return $this->render('@App/pages/projects.html.twig');
-    }
-
-    /**
-     * @Route("/what-we-offer", name="what_we_offer")
-     */
-    public function whatWeOfferAction(){
-        return $this->render('@App/pages/whatWeOffer.html.twig');
-    }
-
-    /**
-     * @Route("/packages", name="packages")
-     */
-    public function packagesAction(){
-        return $this->render('@App/pages/packages.html.twig');
-    }
-
-    /**
-     * @Route("/promos", name="promos")
-     */
-    public function promosAction(){
-        return $this->render('@App/pages/promos.html.twig');
-    }
-
-    /**
-     * @Route("/how-to-buy", name="how_to_buy")
-     */
-    public function howToBuyAction(){
-        return $this->render('@App/pages/howToBuy.html.twig');
-    }
-
-    /**
      * @Route("/blog", name="blog")
      */
     public function blogAction(){
@@ -70,4 +35,14 @@ class DefaultController extends Controller
     public function aboutUsAction(Request $request){
         return $this->render('@App/pages/aboutUs.html.twig');
     }
+
+    /**
+     * @Route("/location", name="location")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function mapAction(Request $request){
+        return $this->render('@App/Map/map.html.twig');
+    }
+
 }
