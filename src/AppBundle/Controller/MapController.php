1497688a52db51da550e953a2ce9079b028648f5
Yohualli <tuguali19@ciencias.unam.mx>
2016-04-18 17:40:25 -0500

    encontrar puntos mediante tabla y medicion de distancia

---
+++ src/AppBundle/Controller/MapController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yohualli
+ * Date: 18/04/16
+ * Time: 03:18 PM
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Util\PdoUtil;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MapController extends Controller
+{
+
+    /**
+     * @Route("/distance", name="distance_between_points")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function distanceBetweenPointsAction(Request $request){
+
+        $em = $this-> getDoctrine()->getManager();
+
+        $lat = $request->get('lat');
+        $lng = $request->get('lng');
+        $stmt = "SELECT p.id ".
+            "from points p ".
+            "where ROUND(6353 * 2 * ASIN(SQRT( POWER(SIN((p.lat - abs($lat)) * pi()/180 / 2),2) ".
+            "+ COS(p.lat * pi()/180 ) * COS(abs($lat) *  pi()/180) * POWER(SIN((p.lng - $lng) ".
+            "*  pi()/180 / 2), 2) )), 2) <= 1";
+
+        $points = PdoUtil::selectFromCursor($em->getConnection(),$stmt);
+
+        return new JsonResponse($points);
+    }
+}
